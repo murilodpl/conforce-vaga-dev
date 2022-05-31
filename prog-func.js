6d3ab29138715cc285bffc7c41af2e01eb020c53
Murilo <inayievmpl@gmail.com>
2022-05-31 09:12:58 -0300

    feat: funções lógica de programação

---
+++ prog-func.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Faça uma função para calcular o dobro de um número qualquer
+ */
+function doubleNum(n) {
+    return n * 2;
+}
+
+/**
+ * 2. Considerando que todos os meses tenham 30 dias, calcular o total de dias de n meses
+ */
+function countMonth(n) {
+    return n * 30;
+}
